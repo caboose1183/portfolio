c8257d216b639e03f29beb805bc0fedad50b73d2
Sidney Pang <sidneypang@hotmail.co.uk>
2023-04-24 01:00:54 -0400

    zoomed in camera, changed font size of name

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -5,12 +5,21 @@ import {
   PresentationControls,
   ContactShadows,
   Html,
+  Text,
 } from "@react-three/drei";
+
+import { useThree, useFrame } from "@react-three/fiber";
 
 export default function Experience() {
   const computer = useGLTF(
     "https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/macbook/model.gltf"
   );
+
+  const zoom = () => {
+    useThree(({ camera }) => {
+      camera.position.set(1, 1, 1);
+    });
+  };
 
   return (
     <>
@@ -22,12 +31,24 @@ export default function Experience() {
         global
         rotation={[0.13, 0.1, 0]}
         polar={[-0.4, 0.2]}
-        azimuth={[-1, 0.75]}
+        azimuth={[-0.2, 0.75]}
         config={{ mass: 2, tension: 400 }}
         snap={{ mass: 4, tension: 400 }}
       >
         <Float rotationIntensity={0.4}>
+          {/* screen light */}
+          <rectAreaLight
+            width={2.5}
+            height={1.65}
+            intensity={65}
+            color={"#6a18ee"}
+            rotation={[-0.1, Math.PI, 0]}
+            position={[0, 0.55, -1.15]}
+          />
+
+          {/* computer model */}
           <primitive object={computer.scene} position-y={-1.2}>
+            {/* holds iframe to generate site on seperate screenm mimicing laptop screen */}
             <Html
               transform
               wrapperClass="htmlScreen"
@@ -38,6 +59,17 @@ export default function Experience() {
               <iframe src="https://caboose1183.netlify.app/"></iframe>
             </Html>
           </primitive>
+
+          {/* name */}
+          <Text
+            font="./bubblegum-sans-v16-latin-regular.woff"
+            fontSize={0.4}
+            position={[1, 1, 0.75]}
+            rotation-y={-1.25}
+            maxWidth={2}
+          >
+            Sidney Pang
+          </Text>
         </Float>
       </PresentationControls>
 
